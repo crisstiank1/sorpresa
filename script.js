f557744e6c27d484485d4c62f6e93aa86e933066
crisstiank1 <cristiancuentaff11@gmail.com>
2025-04-11 20:21:14 -0500

    Agregando

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnRandom = document.querySelector("#random-btn")
+
+function moverAleatoreamente(btn){
+    btn.style.fontWeight = "bolder";
+    btn.style.position = "absolute";
+    btn.style.top = Math.floor(Math.random()*90+5)+ "%";
+    btn.style.left = Math.floor(Math.random()*90+5)+ "%";
+}
+
+btnRandom.addEventListener("mouseenter", function(e){
+    moverAleatoreamente(e.target)
+})
